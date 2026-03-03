83ca639bceb51ae13d2ae91edfa185279821aa42
VivIsBee <averse.abfun@gmail.com>
2026-03-03 15:45:05 -0600

    Update remove-content-label.user.js

--- remove-content-label.user.js
+++ remove-content-label.user.js
@@ -4,9 +4,17 @@
 // @version      2026-03-03
 // @description  removes tumblr's content label cover things
 // @author       @queeeeeeeeeeeeeeeeeeeeeeeeeeer
-// @match        https://www.tumblr.com/
+// @match        https://*.tumblr.com/*
 // @icon         https://raw.githubusercontent.com/VivIsBee/remove-tumblr-content-label/refs/heads/main/icon.png
 // @grant        none
+// @updateURL    https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.meta.js
+// @downloadURL  https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.user.js
+// @supportURL   https://github.com/VivIsBee/remove-tumblr-content-label
+// @homepage     https://github.com/VivIsBee/remove-tumblr-content-label
+// @sandbox      DOM
+// @tag          social media
+// @tag          utilities
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
