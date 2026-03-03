9d7be9c83c542d752d88840bcf168d34235a1c23
VivIsBee <averse.abfun@gmail.com>
2026-03-03 15:44:54 -0600

    Create remove-content-label.meta.js

---
+++ remove-content-label.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Remove Content Label
+// @namespace    https://verygay.fyi
+// @version      2026-03-03
+// @description  removes tumblr's content label cover things
+// @author       @queeeeeeeeeeeeeeeeeeeeeeeeeeer
+// @match        https://*.tumblr.com/*
+// @icon         https://raw.githubusercontent.com/VivIsBee/remove-tumblr-content-label/refs/heads/main/icon.png
+// @grant        none
+// @updateURL    https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.meta.js
+// @downloadURL  https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.user.js
+// @supportURL   https://github.com/VivIsBee/remove-tumblr-content-label
+// @homepage     https://github.com/VivIsBee/remove-tumblr-content-label
+// @sandbox      DOM
+// @tag          social media
+// @tag          utilities
+// @run-at       document-idle
+// ==/UserScript==
