f326ded3175339b85aa1449b0ff48d4129126cc6
VivIsBee <averse.abfun@gmail.com>
2026-03-09 21:25:42 -0500

    Update remove-content-label.user.js

--- remove-content-label.user.js
+++ remove-content-label.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Remove Content Label
 // @namespace    https://verygay.fyi
-// @version      2026-03-04
+// @version      2026-03-09
 // @description  removes tumblr's content label cover things
 // @author       @queeeeeeeeeeeeeeeeeeeeeeeeeeer
-// @match        https://*tumblr.com/*
+// @include      https://tumblr.com/*
+// @include      https://*.tumblr.com/*
 // @icon         https://raw.githubusercontent.com/VivIsBee/remove-tumblr-content-label/refs/heads/main/icon.png
 // @grant        none
 // @updateURL    https://github.com/VivIsBee/remove-tumblr-content-label/raw/refs/heads/main/remove-content-label.meta.js
